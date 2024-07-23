d995efa7526fdae6f0b89446833e317bf3fd7207
Dom <leodomsolar@gmail.com>
2024-07-23 16:51:56 +0700

    Commit

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,24 +1,77 @@
-import React, { useEffect, useState } from "react";
-import Layout from "../components/Layout";
-import CKEditor from "../components/CKEditor";
+import React, { useState } from 'react';
+import './App.css';
 
-function Home() {
-    const [content, setContent] = useState('');
+const initialBoard = Array(9).fill(null);
 
-    const submitCreate = () => {
-        console.log(content);
-    };
+function App() {
+  const [board, setBoard] = useState(initialBoard);
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [winner, setWinner] = useState(null);
+  const [isDraw, setIsDraw] = useState(false);
 
-    return (
-        <Layout>
-            <div>
-                <CKEditor value={content} onBlur={setContent} />
-                <button className="btn btn-primary" onClick={submitCreate}>
-                    Tạo
-                </button>
-            </div>
-        </Layout>
-    );
+  const handleCellClick = (index) => {
+    if (board[index] || winner || isDraw) return;
+
+    const newBoard = board.slice();
+    newBoard[index] = currentPlayer;
+    setBoard(newBoard);
+
+    const newWinner = calculateWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    } else if (!newBoard.includes(null)) {
+      setIsDraw(true);
+    } else {
+      setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+    }
+  };
+
+  const calculateWinner = (board) => {
+    const winningCombinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+      [0, 4, 8], [2, 4, 6] // diagonals
+    ];
+
+    for (let combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+
+    return null;
+  };
+
+  const resetGame = () => {
+    setBoard(initialBoard);
+    setCurrentPlayer('X');
+    setWinner(null);
+    setIsDraw(false);
+  };
+
+  return (
+    <div className="container text-center">
+      <h1 className="my-4">Tic Tac Toe</h1>
+      <div id="gameBoard" className="d-flex flex-wrap justify-content-center">
+        {board.map((cell, index) => (
+          <div
+            key={index}
+            className={`cell ${cell ? cell.toLowerCase() : ''}`}
+            onClick={() => handleCellClick(index)}
+          >
+            {cell}
+          </div>
+        ))}
+      </div>
+      <div id="gameStatus" className="my-4">
+        {winner ? `Player ${winner} wins!` : isDraw ? "It's a draw!" : `Player ${currentPlayer}'s turn`}
+      </div>
+      <button id="resetButton" className="btn btn-primary" onClick={resetGame}>
+        Reset Game
+      </button>
+    </div>
+  );
 }
 
-export default Home;
+export default App;
